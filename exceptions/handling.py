a9e5cd542108fe3bc63aafe62e43dc30f6421100
kbbn-debugger <baul.kevinlloyd@gmail.com>
2025-11-23 17:47:04 +0800

    Register Handling Exception - 2
    I forgot to save the changes.
    
    Register Handling Exception Errors
    
    Registered the following Handling Exception Errors:
    HEX000 - No outliers found
    HEX001- Unknown, not allowed, or invalid methods
    HEX003 - Winsorization produced invalid results
    
    exception/base.py
    changed detector to method.
    
    exception/columns.py
    changed detector to method in the passed super().__init__
    
    edit from the previous commit:
    .gitignore is updated

--- exceptions/handling.py
+++ exceptions/handling.py
@@ -2,11 +2,41 @@ from base import OutliPyException
 from typing import Optional, List
 from error_codes import ErrorCodeRegistry
 
+ErrorCodeRegistry.register(
+    "HEX000",
+    "[OutliPy Handling] - {error_code}\n\n"
+    "No outliers found for handling, but handling was forced\n\n"
+    "Suggestion: {suggestion}"
+)
+
+ErrorCodeRegistry.register(
+    "HEX001",
+    "[OutliPy Handling] - {error_code}\n\n" \
+    "{method} is unknown, invalid, or not allowed.\n\n"
+    "These are the following allowed methods:\n{allowed_methods}\n\n"
+    "Suggestion: {suggestion}"
+)
+
+ErrorCodeRegistry.register(
+    "HEX002",
+    "[OutliPy Handling] - {error_code}\n\n"
+    "Replacement value {fill_value} is invalid or out of bounds.'n'n"
+    "Suggestion: {suggestion}"
+)
+
+ErrorCodeRegistry.register(
+    "HEX003",
+    "[OutliPy Handling] - {error_code}\n\n"
+    "Winsorization produced invalid results"
+    "Suggestion: {suggestion}"
+)
 
 class HandlingException(OutliPyException):
     """
     Raised when outlier handling (removal, replacement, winsorization, etc.) fails
     due to invalid strategy, configuration, or data issues.
     """
-    default_error_code = "HE000"
+    default_error_code = "HEX000"
+
+
     pass
